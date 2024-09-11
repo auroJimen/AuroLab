b0f323cceefeda6f7974f922daa6f8161917cf2c
Aurora Jimenez <auro.jimenezr@gmail.com>
2024-09-11 09:51:51 +0200

    Logs (backup) update

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -1,10 +1,11 @@
 var searchData=
 [
   ['clearbuffer_0',['clearBuffer',['../classbuffer___class.html#a6b423be6bc54130652e70f0e9010b619',1,'buffer_Class']]],
-  ['contact_1',['Contact',['../md_src_2_r_e_a_d_m_e___t_e_m_p_l_a_t_e.html#autotoc_md11',1,'']]],
-  ['contributing_2',['Contributing',['../md_src_2_r_e_a_d_m_e___t_e_m_p_l_a_t_e.html#autotoc_md8',1,'']]],
-  ['contributors_3a_3',['Top contributors:',['../md_src_2_r_e_a_d_m_e___t_e_m_p_l_a_t_e.html#autotoc_md9',1,'']]],
-  ['coord_4',['coord',['../structcoord.html',1,'']]],
-  ['current_20feature_20roadmap_5',['Current feature roadmap',['../md_readme.html#autotoc_md0',1,'']]],
-  ['cursor_6',['cursor',['../classbuffer___class.html#a3ab4b8088883189f50066d32c59f3761',1,'buffer_Class']]]
+  ['connecttonetwork_1',['connectToNetWork',['../classwifi_menu___class.html#ad647b8ab84c06993815b6eca2c218d9c',1,'wifiMenu_Class']]],
+  ['contact_2',['Contact',['../md_src_2_r_e_a_d_m_e___t_e_m_p_l_a_t_e.html#autotoc_md11',1,'']]],
+  ['contributing_3',['Contributing',['../md_src_2_r_e_a_d_m_e___t_e_m_p_l_a_t_e.html#autotoc_md8',1,'']]],
+  ['contributors_3a_4',['Top contributors:',['../md_src_2_r_e_a_d_m_e___t_e_m_p_l_a_t_e.html#autotoc_md9',1,'']]],
+  ['coord_5',['coord',['../structcoord.html',1,'']]],
+  ['current_20feature_20roadmap_6',['Current feature roadmap',['../md_readme.html#autotoc_md0',1,'']]],
+  ['cursor_7',['cursor',['../classbuffer___class.html#a3ab4b8088883189f50066d32c59f3761',1,'buffer_Class']]]
 ];
--- docs/html/search/all_4.js
+++ docs/html/search/all_4.js
@@ -3,5 +3,5 @@ var searchData=
   ['elementname_0',['elementName',['../classlist___class.html#ae578e30c59f80e55bef2b709e885c89d',1,'list_Class']]],
   ['elementnum_1',['elementNum',['../classlist___class.html#a1343001263263df83a0e2a09e528e7bb',1,'list_Class']]],
   ['enabletextinput_2',['enableTextInput',['../classtext_box___class.html#a4f508e0fd1645f5209b3dce4459adb07',1,'textBox_Class']]],
-  ['enterevent_3',['enterEvent',['../classlist___class.html#ab2676ecc5c076b8159249d2726f7707d',1,'list_Class::enterEvent()'],['../classwifi_menu___class.html#a12863a3752876d179d948879a75362a1',1,'wifiMenu_Class::enterEvent()']]]
+  ['enterevent_3',['enterEvent',['../classlist___class.html#ae72807af43934ff2686d131eec10d51b',1,'list_Class::enterEvent()'],['../classwifi_menu___class.html#af7a7bccf484d37d27a114c52429c6572',1,'wifiMenu_Class::enterEvent()']]]
 ];
