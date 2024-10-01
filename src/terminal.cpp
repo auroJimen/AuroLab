35e2d484f0ff0312f2370cee04caf009e86e223e
Aurora Jimenez <auro.jimenezr@gmail.com>
2024-10-01 12:32:20 +0200

    Fixed dep tree to star work on terminal app

--- src/terminal.cpp
+++ src/terminal.cpp
@@ -1,2 +1,13 @@
 //Contains fuctions related to the backend of the terminal app
 //Should support ssh, possibly telnet, serial over usb and serial over uart trough port a
+
+//Main screen should let user select the kind of terminal they wanna use
+//Must work out the split between cores for the app 
+//(gui should be processed @ 0 while the backend stuff should be @ one)
+
+
+//Includes
+#include "GUI.h" //Must be able to call the GUI library to write to screen
+#include "libssh_esp32.h" //SSH libraries used
+#include <libssh/libssh.h> //SSH libraries used
+
